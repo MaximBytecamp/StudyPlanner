26785c19731e24b70d20eb1653d7c36b2010e144
Maxim Makarov <mmaxim01@yandex.ru>
2026-07-06 19:37:07 +0300

    изменения

--- study_planner/config.py
+++ study_planner/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path 
 
-BASE_DIR = Path(__file__).resolve().parent.parent 
-TASKS_FILE = BASE_DIR / "tasks.json"
+BASE_DIR = Path(__file__).resolve().parent.parent  #C:\Users\teacher\Desktop\StudyPlanner\study_planner\config.py
+TASKS_FILE = BASE_DIR / "tasks.json"  #C:\Users\teacher\Desktop\StudyPlanner\tasks.json
 
 #study_planner/config.py -> C:Users/Maxim/StudyPlanner/study_planner/config.py 
 # -> .parent -> C:Users/Maxim/StudyPlanner/study_planner/ -> .parent -> C:Users/Maxim/StudyPlanner/
